8794734a6dd6b5324d23ecba1e87d850b45749b8
Priti <pritiyelutla@gmail.com>
2025-11-17 18:24:17 +0530

    Initial project setup

---
+++ src/test/java/com/spw/field_Investigation_system/FieldInvestigationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spw.field_Investigation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldInvestigationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
